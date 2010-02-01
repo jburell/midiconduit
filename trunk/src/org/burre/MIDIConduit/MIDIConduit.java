85526be519e7f7266cf261e39a41f8e1d748a3bc
johan.burell <johan.burell@6f291256-e804-11de-91d6-89caad0352f8>
2010-02-01 13:06:50 +0000

    Changed some naming conventions and added a JARDesc-file for easy JAR exports.

--- trunk/src/org/burre/MIDIConduit/MIDIConduit.java
+++ trunk/src/org/burre/MIDIConduit/MIDIConduit.java
@@ -24,12 +24,12 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class MIDIConduit{
-	private TrayIcon trayIcon;
-	private static CheckboxMenuItem[] inDeviceCBList;
-	private static CheckboxMenuItem[] outDeviceCBList;
-	public PopupMenu popup;
-	public SystemTray tray;
-	public MIDIRoute route;
+	private TrayIcon m_trayIcon;
+	private static CheckboxMenuItem[] m_inDeviceCBList;
+	private static CheckboxMenuItem[] m_outDeviceCBList;
+	public PopupMenu m_popup;
+	public SystemTray m_tray;
+	public MIDIRoute m_route;
 
 	public static void main(String[] args){
 		/* Use an appropriate Look and Feel */
@@ -61,10 +61,10 @@ public class MIDIConduit{
 	}
 
 	public MIDIConduit(){
-		popup = new PopupMenu();
-		tray = SystemTray.getSystemTray();
-		trayIcon = new TrayIcon(createImage("/images/note.png", "tray icon"));
-		route = new MIDIRoute(trayIcon);
+		m_popup = new PopupMenu();
+		m_tray = SystemTray.getSystemTray();
+		m_trayIcon = new TrayIcon(createImage("/images/note.png", "tray icon"));
+		m_route = new MIDIRoute(m_trayIcon);
 	}
 
 	public class StateChangedListener implements ItemListener{
@@ -80,9 +80,9 @@ public class MIDIConduit{
 					//inDeviceCBList[i].setState(false);
 					}else{
 						if(menu.getLabel() == "Output"){
-							route.setSelectedOutDevice(i);
+							m_route.setSelectedOutDevice(i);
 						}else if(menu.getLabel() == "Input"){
-							route.setSelectedInDevice(i);
+							m_route.setSelectedInDevice(i);
 						}
 						((CheckboxMenuItem)menu.getItem(i)).setState(true);
 					}
@@ -94,7 +94,7 @@ public class MIDIConduit{
 	public class ExitActionListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e){
-			tray.remove(trayIcon);
+			m_tray.remove(m_trayIcon);
 			System.exit(0);
 		}
 	}
@@ -106,11 +106,11 @@ public class MIDIConduit{
 			if("Test MIDI output".equals(item.getLabel())){
 				try{
 					for(int j = 0; j < 128; j += 1){
-						route.writeMIDIMessage(ShortMessage.CONTROL_CHANGE, (byte)0, (byte)1, (byte)j);
+						m_route.writeMIDIMessage(ShortMessage.CONTROL_CHANGE, (byte)0, (byte)1, (byte)j);
 						Thread.sleep(10);
 					}
 				}catch(InterruptedException e1){
-					trayIcon.displayMessage(
+					m_trayIcon.displayMessage(
 						"Error",
 						e1.getMessage(),
 						TrayIcon.MessageType.ERROR);
@@ -120,8 +120,8 @@ public class MIDIConduit{
 	}
 
 	public static void createAndShowGUI(MIDIConduit conduit){
-		Vector<Info> inDevices = conduit.route.getInDevices();
-		Vector<Info> outDevices = conduit.route.getOutDevices();
+		Vector<Info> inDevices = conduit.m_route.getInDevices();
+		Vector<Info> outDevices = conduit.m_route.getOutDevices();
 
 		//openMIDI();
 
@@ -132,8 +132,8 @@ public class MIDIConduit{
 		}
 
 		// Init checkbox lists
-		inDeviceCBList = new CheckboxMenuItem[inDevices.size()];
-		outDeviceCBList = new CheckboxMenuItem[outDevices.size()];
+		m_inDeviceCBList = new CheckboxMenuItem[inDevices.size()];
+		m_outDeviceCBList = new CheckboxMenuItem[outDevices.size()];
 
 		// Create a popup menu components
 		Menu inMenu = new Menu("Input");
@@ -144,43 +144,43 @@ public class MIDIConduit{
 		MenuItem midiItem = new MenuItem("Test MIDI output");
 
 		// Add components to popup menu
-		conduit.popup.add(inMenu);
-		conduit.popup.add(outMenu);
-		conduit.popup.add(midiItem);
-		conduit.popup.addSeparator();
-		conduit.popup.add(exitItem);
+		conduit.m_popup.add(inMenu);
+		conduit.m_popup.add(outMenu);
+		conduit.m_popup.add(midiItem);
+		conduit.m_popup.addSeparator();
+		conduit.m_popup.add(exitItem);
 
 		// Populate input MIDI list
 		for(int i = 0; i < inDevices.size(); i++){
 			CheckboxMenuItem cb = new CheckboxMenuItem(inDevices.elementAt(i).getName());
-			inDeviceCBList[i] = cb;
+			m_inDeviceCBList[i] = cb;
 			cb.addItemListener(conduit.new StateChangedListener());
 
 			inMenu.add(cb);
 		}
-		CheckboxMenuItem netCheckBox = new CheckboxMenuItem("Network (" + conduit.route.getMIDISocketMgr().getAdress().getHostAddress() + ")");
+		CheckboxMenuItem netCheckBox = new CheckboxMenuItem("Network (" + conduit.m_route.getMIDISocketMgr().getAdress().getHostAddress() + ")");
 		netCheckBox.addItemListener(conduit.new StateChangedListener());
 		inMenu.add(netCheckBox);
 
 		// Populate output MIDI list
 		for(int i = 0; i < outDevices.size(); i++){
 			CheckboxMenuItem cb = new CheckboxMenuItem(outDevices.elementAt(i).getName());
-			outDeviceCBList[i] = cb;
+			m_outDeviceCBList[i] = cb;
 			cb.addItemListener(conduit.new StateChangedListener());
 
 			outMenu.add(cb);
 		}
 
-		conduit.trayIcon.setPopupMenu(conduit.popup);
+		conduit.m_trayIcon.setPopupMenu(conduit.m_popup);
 
 		try{
-			conduit.tray.add(conduit.trayIcon);
+			conduit.m_tray.add(conduit.m_trayIcon);
 		}catch(AWTException e){
 			System.out.println("TrayIcon could not be added.");
 			return;
 		}
 
-		conduit.trayIcon.addActionListener(new ActionListener(){
+		conduit.m_trayIcon.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				JOptionPane.showMessageDialog(
 					null,
